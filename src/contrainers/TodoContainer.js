c5d22312a0eb63b2cc3c0bd91de035cd8387b19c
Aekachai <sniperaek@gmail.com>
2018-07-11 17:04:54 +0700

    add components

---
+++ src/contrainers/TodoContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p> Todo list</p>
+      </div>
+    );
+  }
+}
+
+export default App;
